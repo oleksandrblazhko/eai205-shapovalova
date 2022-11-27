9f5343c14c27f9123459174e3ee26d87b15a4d68
bastard2 <79440953+bastard2@users.noreply.github.com>
2022-11-27 22:18:50 +0200

    Update ProgramCodeModified.sql

--- 3- SoftwareConstruction/3.3-ContinuousIntegration/ProgramCodeModified.sql
+++ 3- SoftwareConstruction/3.3-ContinuousIntegration/ProgramCodeModified.sql
@@ -1,9 +1,8 @@
-CREATE TABLE Користувачка(
-id_korystuvachka int,
-Прізвище varchar,
-Імя varchar,
-Вік int,
-Інвалідність int,
-Номер_телефону char,
+CREATE TABLE Танець(
+id_dance SERIAL int,
+Назва varchar,
+Країна varchar,
+Інтенсивність int,
+Додаткова_інформація varchar(1500),
 id_dodatok int references Додаток(id_dodatok) ON DELETE CASCADE
 );
