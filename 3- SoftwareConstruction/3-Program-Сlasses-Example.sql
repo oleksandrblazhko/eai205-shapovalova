e6207050b9711e90b84037185b1a02be1d3f687e
bastard2 <79440953+bastard2@users.noreply.github.com>
2022-11-27 21:02:58 +0200

    Update 3-Program-Сlasses-Example.sql

--- 3- SoftwareConstruction/3-Program-Сlasses-Example.sql
+++ 3- SoftwareConstruction/3-Program-Сlasses-Example.sql
@@ -1,11 +1,6 @@
-CREATE OR REPLACE TYPE Танець UNDER Інформація and Відео (
- id_dance SERIAL PRIMARY KEY,
-Назва varchar,
-Додаткова_інформація varchar(1500),
-      OVERRIDING MEMBER PROCEDURE display 
-);
-CREATE OR REPLACE TYPE Інформація (
- d_informacion SERIAL PRIMARY KEY,
+
+CREATE OR REPLACE TYPE Інформація AS OBJECT (
+ d_informacion SERIAL INT,
 Дата_започаткування date,
 Країна varchar,
 Інтенсивність int,
@@ -15,13 +10,14 @@ CREATE OR REPLACE TYPE Інформація (
       OVERRIDING MEMBER PROCEDURE display 
 );
 CREATE OR REPLACE TYPE Відео AS OBJECT (
-  id_video SERIAL PRIMARY KEY,
+  id_video SERIAL INT,
 Назва varchar,
 Розмір_файлу int,
 Формат char,
 Тривалість int,
       MEMBER PROCEDURE display
 ); 
+
 CREATE OR REPLACE TYPE Користувачка AS OBJECT (
   id_korystuvachka SERIAL PRIMARY KEY,
 Прізвище varchar,
@@ -32,21 +28,18 @@ CREATE OR REPLACE TYPE Користувачка AS OBJECT (
   
       MEMBER PROCEDURE display
 ); 
+CREATE OR REPLACE TYPE Танець UNDER Інформація (
+ id_dance SERIAL PRIMARY KEY,
+Назва varchar,
+Додаткова_інформація varchar(1500),
+      OVERRIDING MEMBER PROCEDURE display 
+);
 CREATE OR REPLACE TYPE Застосунок_для_танців UNDER Танець AS OBJECT (
-  id_dodatok SERIAL PRIMARY KEY,
+  id_dodatok SERIAL INT,
 Назва varchar,
 Тип varchar,
 Розмір float,
 Мінімальні_характ_для_запуску  varchar(300),
 Зміст  varchar(300),
       MEMBER PROCEDURE display
-  CONSTRUCTOR FUNCTION Відео(Назва varchar)
-          RETURN SELF AS RESULT,
-        /* Функції отримання значень атрибутів */
-    MEMBER FUNCTION get_Informacion RETURN NUMBER,
-    MEMBER FUNCTION get_Video RETURN VARCHAR,
-    MEMBER FUNCTION get_price RETURN NUMBER,
-  /* Процедура виводу на екран значень атрибутів */
-      MEMBER PROCEDURE display
-); 
-
+  )
